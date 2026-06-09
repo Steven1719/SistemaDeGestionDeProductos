c85d0d33aaa484b69a01d732b348c46fef262e06
Steven1719 <freddystiven759@gmail.com>
2026-06-08 21:16:28 -0600

    commit 8 de junio

--- Producto.cs
+++ Producto.cs
@@ -1,4 +1,49 @@
-public abstract class Producto
-{
-    
+public abstract class Producto {  
+        private decimal precio;
+        private int cantidadEnStock;
+
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+
+        public decimal Precio
+        {
+            get { return precio; }
+            set 
+            { 
+                if (value >= 0)
+                {
+                    precio = value;
+                }
+                else
+                {
+                    throw new ArgumentException("El precio no puede ser un valor negativo.");
+                }
+            }
+        }
+
+        public int CantidadEnStock
+        {
+            get { return cantidadEnStock; }
+            set 
+            { 
+                if (value >= 0)
+                {
+                    cantidadEnStock = value;
+                }
+                else
+                {
+                    throw new ArgumentException("La cantidad en stock no puede ser negativa.");
+                }
+            }
+        }
+
+        protected Producto(string id, string nombre, decimal precio, int cantidadEnStock)
+        {
+            Id = id;
+            Nombre = nombre;
+            Precio = precio;
+            CantidadEnStock = cantidadEnStock;
+        }
+
+        public abstract void MostrarDetalles();
 }
